111a05d543e3344c7464e72ce826a0011af3f054
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-12-01 17:24:03 -0500

    Exercism simplify allergies class

--- exercism/allergies.py
+++ exercism/allergies.py
@@ -11,26 +11,13 @@ class Allergies(object):
     )
 
     def __init__(self, score):
-        self.__lst = self.__process_score(score % 256)
-
-    # I don't think using @property provides any value here.
-    @property
-    def lst(self):
-        return self.__lst
-
-    def is_allergic_to(self, item):
-        return item in self.__lst
-
-    # Can't take credit for using & because I copied the solution.
-    # Don't know how else I could solve this without my old approach.
-    def __process_score(self, score):
-        """
-        Credit goes to treyhunner on github for this approach using bitwise AND
-        """
-        return [
+        self.lst = [
             item
             for i, item in enumerate(self.ALLERGENS)
             if score & 1<<i
         ]
+
+    def is_allergic_to(self, item):
+        return item in self.lst
 
 
