b23d3d3a6ade1de9d78f5d4d2ebc152153bc14cb
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-12-10 14:00:47 -0500

    Implement tests

--- Interview-Practice/box_test.py
+++ Interview-Practice/box_test.py
@@ -3,13 +3,17 @@ from box import get_box_coordinates
 
 class GetBoxCoordinatesTest(unittest.TestCase):
     def test_no_box_is_present(self):
-        pass
+        self.assertEqual(get_box_coordinates(image_seven), None)
 
     def test_box_is_not_on_edge(self):
-        pass
+        self.assertEqual(get_box_coordinates(image_one), { "start": (0 , 0), "end": (0 , 0) })
+        self.assertEqual(get_box_coordinates(image_two), { "start": (0 , 0), "end": (6 , 6) })
+        self.assertEqual(get_box_coordinates(image_three), { "start": (2 , 3), "end": (3 , 5) })
 
     def test_box_is_present_and_on_edge(self):
-        pass
+        self.assertEqual(get_box_coordinates(image_four), { "start": (2 , 4), "end": (4 , 5) })
+        self.assertEqual(get_box_coordinates(image_five), { "start": (2 , 4), "end": (3 , 6) })
+        self.assertEqual(get_box_coordinates(image_six), { "start": (2 , 4), "end": (4 , 6) })
 
 if __name__ == "__main__":
     unittest.main()
