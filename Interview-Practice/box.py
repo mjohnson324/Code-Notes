4adeb9f3ecbc26d8dabd258cb339be6f5292faeb
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-12-10 13:57:58 -0500

    Implement box problem and test framework

---
+++ Interview-Practice/box.py
@@ -0,0 +1,15 @@
+# You have an image represented by a 2-D array of 1s and 0s. The 0s represent a box.
+# Find the coordinates of the box
+# There is only one box.
+# The box and array are rectangular and can vary in size.
+# sample input: [
+#     [1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 1, 1, 1, 1],
+#     [1, 1, 1, 0, 0, 0, 1],
+#     [1, 1, 1, 0, 0, 0, 1],
+#     [1, 1, 1, 1, 1, 1, 1]
+# ]
+# sample output: { "start": (2, 3), "end": (4, 5) }
+
+def get_box_coordinates(image):
+    pass
