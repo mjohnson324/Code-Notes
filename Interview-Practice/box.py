34bd0b4e6fdf146603406694e03cec3b7f62613e
mjohnson324 <michaelwilliamjohnson777@gmail.com>
2018-12-10 14:24:57 -0500

    Submit working solution for box

--- Interview-Practice/box.py
+++ Interview-Practice/box.py
@@ -12,4 +12,31 @@
 # sample output: { "start": (2, 3), "end": (4, 5) }
 
 def get_box_coordinates(image):
-    pass
+    # Define needed variables
+    start = None
+    end = None
+    next_row_num = None
+    next_column_num = None
+    last_row = len(image) - 1
+    last_column = len(image[0]) - 1
+    # We must check every number
+    for i, row in enumerate(image):
+        for j, num in enumerate(row):
+            if start is None and num == 0:
+                start = (i, j)
+            # The last 0 will be bordered by 1s in the next row and column, or
+            # the edge of the image. Check these conditions.
+            if i != last_row:
+                next_row_num = image[i + 1][j]
+            if j != last_column:
+                next_column_num = image[i][j + 1]
+            if num == 0:
+                if next_row_num == 1 and next_column_num == 1:
+                    end = (i, j)
+                elif next_row_num == 1 and j == last_column:
+                    end = (i, j)
+                elif i == last_row and next_column_num == 1:
+                    end = (i, j)
+                elif i == last_row and j == last_column:
+                    end = (i, j)
+    return { "start": start, "end": end } if start else None
--- Interview-Practice/box_test.py
+++ Interview-Practice/box_test.py
@@ -2,18 +2,17 @@ import unittest
 from box import get_box_coordinates
 
 class GetBoxCoordinatesTest(unittest.TestCase):
-    def test_no_box_is_present(self):
-        self.assertEqual(get_box_coordinates(image_seven), None)
-
     def test_box_is_not_on_edge(self):
         self.assertEqual(get_box_coordinates(image_one), { "start": (0 , 0), "end": (0 , 0) })
-        self.assertEqual(get_box_coordinates(image_two), { "start": (0 , 0), "end": (6 , 6) })
-        self.assertEqual(get_box_coordinates(image_three), { "start": (2 , 3), "end": (3 , 5) })
+        self.assertEqual(get_box_coordinates(image_two), { "start": (2 , 3), "end": (3 , 5) })
 
     def test_box_is_present_and_on_edge(self):
+        self.assertEqual(get_box_coordinates(image_three), { "start": (0 , 0), "end": (4 , 6) })
         self.assertEqual(get_box_coordinates(image_four), { "start": (2 , 4), "end": (4 , 5) })
         self.assertEqual(get_box_coordinates(image_five), { "start": (2 , 4), "end": (3 , 6) })
-        self.assertEqual(get_box_coordinates(image_six), { "start": (2 , 4), "end": (4 , 6) })
+
+    def test_no_box_is_present(self):
+        self.assertEqual(get_box_coordinates(image_six), None)
 
 if __name__ == "__main__":
     unittest.main()
@@ -27,19 +26,19 @@ image_one = [
 ]
 
 image_two = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]
-]
-
-image_three = [
     [1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 0, 0, 0, 1],
     [1, 1, 1, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1]
+]
+
+image_three = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
 ]
 
 image_four = [
@@ -59,14 +58,6 @@ image_five = [
 ]
 
 image_six = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 0, 0, 0],
-    [1, 1, 1, 1, 0, 0, 0],
-    [1, 1, 1, 1, 0, 0, 0]
-]
-
-image_seven = [
     [1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1],
