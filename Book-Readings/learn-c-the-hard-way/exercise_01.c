74ba62c2542171df3d8c33debbab621275b87766
mjohnson324 <michaelwilliamjohnson77&@gmail.com>
2020-05-22 15:25:20 -0400

    lcthw/exercise-1

---
+++ Book-Readings/learn-c-the-hard-way/exercise_01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int distance = 100;
+
+    printf("You are %i miles away.\n", distance);
+
+}
+
+/*
+ Compiles without a return statement
+ Does not compile with a string as the return
+ Compiles without inputs
+ */
